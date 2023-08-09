f2e6442b8c0531066039287fa9effb414a8f561d
Malick NDIAYE <98133571+MalicknND@users.noreply.github.com>
2023-08-09 22:54:03 +0200

    done

--- script.js
+++ script.js
@@ -7,3 +7,38 @@
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+let playOnce = true;
+
+window.addEventListener('scroll', () => {
+    // Navbar Effect 
+   if (window.scrollY > 50){
+    navbar.style.height = "45px"
+   }else {
+    navbar.style.height = "90px"
+   }
+
+   let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+// image 
+   if(scrollValue > 0.45){
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+
+   }
+
+//    popup
+
+if (scrollValue > 0.85 && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOnce = false;
+}
+   console.log(scrollValue);
+})
+
+
+closeBtn.addEventListener('click', () => {
+    popup.style.opacity = 0;
+    popup.style.transform = "translateX(500px";
+})
+console.log(imgImprovise);
